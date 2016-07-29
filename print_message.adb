b4357a04ebeb3b32672c48c510e7a38d16dd4312
Shekin1 <d.delapuente@outlook.com>
2016-07-29 12:51:24 +0200

    Add files via upload

---
+++ print_message.adb
@@ -0,0 +1,33 @@
+with Ada.Text_IO;                  use  Ada.Text_IO;
+with Ada.Integer_Text_IO;
+with Ada.Streams;                  use  Ada.Streams;
+with System.IO;
+
+
+package body DB is
+
+   procedure Print_Message (TM : in Stream_Element_Array) is   
+
+      Message_String : String (1 .. 255);
+
+      function To_String (S : Stream_Element_Array) return String is
+         Result : String (1 .. S'Length);
+      begin
+         for I in Result'Range loop
+            Result (I) :=
+              Character'Val (Stream_Element'Pos
+                             (S (Stream_Element_Offset (I) + S'First - 1)));
+            System.IO.Put_Line (Result);
+         end loop;
+         return Result;
+      end To_String;
+
+   begin
+
+      Message_String := To_String (TM);
+
+      System.IO.Put_Line (Message_String)
+
+   end Print_Message;
+
+end DB;
